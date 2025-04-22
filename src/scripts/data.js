8e338410f93b339a00165660bf58e9cc9ab5f220
takaza-codes <elekovalenko@gmail.com>
2025-04-22 13:35:45 +0300

    add data, font

---
+++ src/scripts/data.js
@@ -0,0 +1,32 @@
+const goodsList = [
+    {
+      id: 1,
+      category: "short",
+      title: "Maple Board - Short",
+      price: 98.00,
+      description: `A compact board that combines functionality and style`,
+      images: [
+        "../assets/images/board1.png"
+      ]
+    },
+    {
+        id: 2,
+        category: "long",
+        title: "Maple Board - Long",
+        price: 125.00,
+        description: `A sleek board that combines functionality and style`,
+        images: [
+          "../assets/images/board2.png"
+        ]
+      },
+      {
+        id: 3,
+        category: "wide",
+        title: "Maple Board - Wide",
+        price: 158.00,
+        description: `A full-sized board that combines functionality and style`,
+        images: [
+          "../assets/images/board3.png"
+        ]
+      },
+]
